7f94ebb8572c12b1ae5b6fd4e8b0285845bc7f6e
Ljrr3045 <ljrr3045@gmail.com>
2025-06-14 17:06:07 -0400

    feature(): adding queries for tx and card holder

---
+++ src/services/queries/getCardHolder/useGetCardHolder.ts
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import { useCardAppContext } from '../../../providers';
+import { API_URL } from '../../../constants';
+import type { IGenericQuery } from '../../../types/globals';
+
+/* Types */
+export type TGetCardHolderProps = {
+  userId: number;
+};
+
+export type TCardHolder = {
+  holderId: number;
+  cardTypeId: number;
+  status: string;
+  statusStr: string;
+  respMsg: string;
+  areaCode: string;
+  mobile: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  birthday: string;
+  country: string;
+  town: string;
+  address: string;
+  postCode: string;
+};
+
+export type TGetCardHolderResponse = {
+  success: boolean;
+  code: number;
+  msg: string;
+  data: TCardHolder[];
+};
+
+/* Hook */
+export const useGetCardHolder = ({
+  userId,
+  onError,
+  enabled,
+}: TGetCardHolderProps & IGenericQuery) => {
+  const { cardAppApiKey } = useCardAppContext();
+
+  return useQuery({
+    queryKey: ['getCardHolder', userId],
+    onError,
+    queryFn: async () => {
+      if (!userId) throw new Error('User ID is missing');
+
+      const response = await axios.get(`${API_URL}/banking/holder/${userId}`, {
+        headers: { 'x-api-key': cardAppApiKey },
+      });
+
+      const cardHolder: TGetCardHolderResponse = response.data;
+      return cardHolder;
+    },
+    enabled: !!enabled,
+  });
+};
---
+++ src/services/queries/getUserTransactions/useGetUserTransactionsById.ts
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import { useCardAppContext } from '../../../providers';
+import { API_URL } from '../../../constants';
+import type { IGenericQuery } from '../../../types/globals';
+import type { TTransaction } from './useGetUserTransactions';
+
+/* Types */
+export type TGetUserTransactionsByIdProps = {
+  id: number;
+  userId: number;
+};
+
+export type TGetUserTransactionsByIdResponse = {
+  success: boolean;
+  code: number;
+  msg: string;
+  data: TTransaction;
+};
+
+/* Hook */
+export const useGetUserTransactionsById = ({
+  id,
+  userId,
+  onError,
+  enabled,
+}: TGetUserTransactionsByIdProps & IGenericQuery) => {
+  const { cardAppApiKey } = useCardAppContext();
+
+  return useQuery({
+    queryKey: ['getUserTransactionsById', userId, id],
+    onError,
+    queryFn: async () => {
+      if (!userId) throw new Error('User ID is missing');
+      if (!id) throw new Error('Transaction ID is missing');
+
+      const response = await axios.get(
+        `${API_URL}/transactions/${userId}/${id}`,
+        {
+          headers: { 'x-api-key': cardAppApiKey },
+        }
+      );
+
+      const transactionsById: TGetUserTransactionsByIdResponse = response.data;
+      return transactionsById;
+    },
+    enabled: !!enabled,
+  });
+};
