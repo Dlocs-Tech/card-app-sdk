a89a21cb014d5914ab4e5781d2cf4adce04131cb
Ljrr3045 <ljrr3045@gmail.com>
2025-06-20 16:41:52 -0400

    feature(deposit): adding call to validate deposit

--- src/services/mutations/cardDeposit/useValidateDeposit.ts
+++ src/services/mutations/cardDeposit/useValidateDeposit.ts
@@ -1,3 +1,49 @@
-export const useValidateDeposit = () => {
-  return {};
+import { useMutation } from '@tanstack/react-query';
+import { useCardAppContext } from '../../../providers';
+import axios from 'axios';
+import { API_URL } from '../../../constants';
+import type { TGenericMutation } from '../../../types/globals';
+
+/* Types */
+export type TValidateDepositProps = {
+  transactionId: string;
+  txHash: string;
+};
+
+export type TValidateDepositResponse = {
+  success: boolean;
+  code: number;
+  msg: string;
+  data: {
+    txId: number;
+  };
+};
+
+/* Hook */
+export const useValidateDeposit = ({
+  onError,
+  onSuccess,
+}: TGenericMutation<TValidateDepositResponse>) => {
+  const { cardAppApiKey } = useCardAppContext();
+  return useMutation<TValidateDepositResponse, Error, TValidateDepositProps>({
+    onError,
+    onSuccess,
+    mutationFn: async ({ transactionId, txHash }: TValidateDepositProps) => {
+      if (!transactionId || !txHash)
+        throw new Error('Transaction ID or TX hash is missing');
+
+      const { data }: { data: TValidateDepositResponse } = await axios.post(
+        `${API_URL}/bridge/validate-deposit/${transactionId}`,
+        { txHash },
+        {
+          headers: {
+            'x-api-key': cardAppApiKey,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      return data;
+    },
+  });
 };
