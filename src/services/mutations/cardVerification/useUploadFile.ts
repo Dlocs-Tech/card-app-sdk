8f23731da5dcddfd545a5a79ebd825a5c04f6063
Ljrr3045 <ljrr3045@gmail.com>
2025-08-10 15:25:44 -0400

    fix(): rollback and adding verify hooks

---
+++ src/services/mutations/cardVerification/useUploadFile.ts
@@ -0,0 +1,49 @@
+import { useMutation } from '@tanstack/react-query';
+import { useCardAppContext } from '../../../providers';
+import axios from 'axios';
+import type { TGenericMutation } from '../../../types/globals';
+
+/* Types */
+export type TUploadFileProps = {
+  file: File;
+};
+
+export type TUploadFileResponse = {
+  code: number;
+  msg: string;
+  success: boolean;
+  data: {
+    fileId: string;
+  };
+};
+
+/* Hook */
+export const useUploadFile = ({
+  onError,
+  onSuccess,
+}: TGenericMutation<TUploadFileResponse>) => {
+  const { cardAppApiKey, cardAppApiUrl } = useCardAppContext();
+
+  return useMutation<TUploadFileResponse, Error, TUploadFileProps>({
+    onError,
+    onSuccess,
+    mutationFn: async ({ file }: TUploadFileProps) => {
+      if (!file) throw new Error('File is required');
+
+      const formData = new FormData();
+      formData.append('file', file);
+
+      const { data }: { data: TUploadFileResponse } = await axios.post(
+        `${cardAppApiUrl}/banking/file/upload`,
+        formData,
+        {
+          headers: {
+            'x-api-key': cardAppApiKey,
+          },
+        }
+      );
+
+      return data;
+    },
+  });
+};
---
+++ src/services/mutations/cardVerification/useVerifyCard.ts
@@ -0,0 +1,70 @@
+import { useMutation } from '@tanstack/react-query';
+import { useCardAppContext } from '../../../providers';
+import axios from 'axios';
+import type { TGenericMutation } from '../../../types/globals';
+
+/* Types */
+export type TVerifyCardProps = {
+  userId: number;
+  title: string;
+  cardNumber: string;
+  fileId: string;
+};
+
+export type TVerifyCardResponse = {
+  code: number;
+  msg: string;
+  success: boolean;
+  data: {
+    merchantOrderNo: string;
+    orderNo: string;
+    title: string;
+    target: string;
+    content: string;
+    tradeType: string;
+    tradeStatus: string;
+    remark: string;
+    createTime: number;
+    updateTime: number;
+  };
+};
+
+/* Hook */
+export const useVerifyCard = ({
+  onError,
+  onSuccess,
+}: TGenericMutation<TVerifyCardResponse>) => {
+  const { cardAppApiKey, cardAppApiUrl } = useCardAppContext();
+
+  return useMutation<TVerifyCardResponse, Error, TVerifyCardProps>({
+    onError,
+    onSuccess,
+    mutationFn: async ({
+      userId,
+      title,
+      cardNumber,
+      fileId,
+    }: TVerifyCardProps) => {
+      if (!userId || !title || !cardNumber || !fileId)
+        throw new Error('User id, title, card number and file id are required');
+
+      const { data }: { data: TVerifyCardResponse } = await axios.post(
+        `${cardAppApiUrl}/v1/banking/work/submit/${userId}`,
+        {
+          title,
+          target: cardNumber,
+          files: [fileId],
+          content: 'Active',
+          tradeType: 'CARD_ACTIVE',
+        },
+        {
+          headers: {
+            'x-api-key': cardAppApiKey,
+          },
+        }
+      );
+
+      return data;
+    },
+  });
+};
