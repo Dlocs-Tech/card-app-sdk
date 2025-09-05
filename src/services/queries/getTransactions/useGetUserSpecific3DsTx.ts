6c35850d069ce3e6fcb53945a24538615d904816
Ljrr3045 <ljrr3045@gmail.com>
2025-09-05 13:11:40 -0400

    feature(transactions): decryting values for specific 3ds tx

--- src/services/queries/getTransactions/useGetUserSpecific3DsTx.ts
+++ src/services/queries/getTransactions/useGetUserSpecific3DsTx.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
+import forge from 'node-forge';
 import { useQuery } from '@tanstack/react-query';
 import { useCardAppContext } from '../../../providers';
 import { STALE_TIME } from '../../../constants';
 import type { TBaseResponse, TGenericQuery } from '../../../types';
 import type { TUser3DsTx } from './useGetUser3DsTxs';
+import { exportPublicKey, generateKeyPair } from '../../../utils';
 
 /* Response */
 export type TGetUserSpecific3DsTxResponse = TBaseResponse & {
@@ -34,8 +36,11 @@ export const useGetUserSpecific3DsTx = ({
       if (!userId || !cardId || !tradeNo)
         throw new Error('User ID, cardId, tradeNo are required');
 
+      const { publicKey: publicKeyPem, privateKey } = generateKeyPair();
+      const publicKey = exportPublicKey(publicKeyPem);
+
       const response = await axios.get(
-        `${cardAppApiUrl}/v2/transactions/${userId}/3ds/${cardId}/${tradeNo}`,
+        `${cardAppApiUrl}/v2/transactions/${userId}/3ds/${cardId}/${tradeNo}?publicKey=${encodeURIComponent(publicKey)}`,
         {
           headers: {
             'x-api-key': cardAppApiKey,
@@ -44,7 +49,18 @@ export const useGetUserSpecific3DsTx = ({
       );
 
       const userSpecific3DsTx: TGetUserSpecific3DsTxResponse = response.data;
-      return userSpecific3DsTx.data;
+      const userSpecific3DsTxData = userSpecific3DsTx.data;
+
+      if (!userSpecific3DsTxData)
+        throw new Error('User specific 3DS tx is missing');
+
+      const encryptedValues = forge.util.decode64(userSpecific3DsTxData.values);
+      const decryptedValues = privateKey.decrypt(encryptedValues, 'RSA-OAEP');
+
+      return {
+        ...userSpecific3DsTxData,
+        values: decryptedValues,
+      };
     },
     enabled: !!enabled,
     staleTime: STALE_TIME,
--- src/utils/card/generateKeys.ts
+++ src/utils/card/generateKeys.ts
@@ -1,7 +1,7 @@
 import forge from 'node-forge';
+
 export function generateKeyPair(bits: number = 2048) {
   const keypair = forge.pki.rsa.generateKeyPair({ bits, e: 0x10001 });
-
   const publicKeyPem = forge.pki.publicKeyToPem(keypair.publicKey);
 
   return {
